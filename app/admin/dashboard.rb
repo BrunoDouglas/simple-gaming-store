c4b46be3753c5f6ea0de3890cc39c04203334a57
Bruno Douglas <brunodouglas@gmail.com>
2018-11-04 18:09:04 -0600

    Implemented ActiveAdming dashboard page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,29 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Latest Orders" do
+          ul do
+            Order.last(5).map do |order|
+              li link_to("# #{order.id} - Customer: #{order.customer.name} - Items: #{order.items.count}", admin_order_path(order))
+            end
+          end
+        end
+    end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+    column do
+      panel "Latest Customers" do
+        ul do
+          Customer.last(5).map do |cust|
+            li link_to("# #{cust.id} - #{cust.name}", admin_customer_path(cust))
+          end
+        end
+      end
+    end
+    end
   end # content
 end
